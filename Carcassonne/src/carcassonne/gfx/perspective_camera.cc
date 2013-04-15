1c9932728ec9736e3a0fa839aa9639553d141a02
Ben Crist <ben@magicmoremagic.com>
2013-04-15 01:09:41 -0500

    camera-mouse raycasting/intersection w/ XZ plane

--- Carcassonne/src/carcassonne/gfx/perspective_camera.cc
+++ Carcassonne/src/carcassonne/gfx/perspective_camera.cc
@@ -24,6 +24,7 @@
 
 #include "carcassonne/gfx/perspective_camera.h"
 
+#include <limits>
 #include <glm/gtc/matrix_transform.hpp>
 
 namespace carcassonne {
@@ -94,14 +95,33 @@ glm::vec3 PerspectiveCamera::windowToWorld(const glm::vec2& window_coords, float
    // TODO: convert window coordinates to ray direction target point using z_near_, right_, top_
    //       then convert the target point from eye coordinates to world coordinates
 
+   glm::vec2 window_normalized(window_coords.x / gfx_cfg_.viewport_size.x,
+                               window_coords.y / gfx_cfg_.viewport_size.y);
+   glm::vec4 target(window_normalized.x * right_ + (1 - window_normalized.x) * left,
+                    window_normalized.y * bottom + (1 - window_normalized.y) * top_,
+                    -1, 1);
+   glm::vec4 target_world(eyeToWorld(target));
+
    // TODO: create ray from camera position and target point above (direction = target - position_)
+
+   glm::vec3 direction(glm::vec3(target_world) - position_);
 
    // TODO: interpolate ray intersection with the plane with constant y-coordinate plane_y
    //       by computing float t = (plane_y - position_.y) / direction.y
    //       then calculate the intersection by multiplying t * direction and adding that to the
    //       camera position.
 
-   return glm::vec3();
+   float delta_y = plane_y - position_.y;
+   float t = -1;
+   if (direction.y != 0)
+      t =  delta_y / direction.y;
+
+   if (t < 0)
+      return glm::vec3(std::numeric_limits<float>::signaling_NaN(),
+                       std::numeric_limits<float>::signaling_NaN(),
+                       std::numeric_limits<float>::signaling_NaN());
+
+   return position_ + direction * t;
 }
 
 } // namespace carcassonne::gfx
